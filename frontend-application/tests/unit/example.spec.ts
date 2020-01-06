e5361131975113a4b12c1701aa082c1eb3996ea8
Klaus Kerstges <klaus.kerstges@mt-ag.com>
2020-01-06 21:03:13 +0100

    add null loader

--- frontend-application/tests/unit/example.spec.ts
+++ frontend-application/tests/unit/example.spec.ts
@@ -2,7 +2,6 @@ import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-// ToDo Unit Tests are Broken due to https://github.com/vuetifyjs/vue-cli-plugin-vuetify/issues/101
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
--- frontend-application/vue.config.js
+++ frontend-application/vue.config.js
@@ -7,5 +7,14 @@ module.exports = {
   ],
   configureWebpack: {
     plugins: [new BundleAnalyzerPlugin()]
+  },
+  chainWebpack: config => {
+    const sassRule = config.module.rule('sass')
+    sassRule.uses.clear()
+    sassRule.use('null-loader').loader('null-loader')
+
+    const scssRule = config.module.rule('scss')
+    scssRule.uses.clear()
+    scssRule.use('null-loader').loader('null-loader')
   }
 }
